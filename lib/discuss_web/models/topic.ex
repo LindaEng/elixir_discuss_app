09db0965ae0922ffee72e049b71db5180fa4e21d
= <90lineng@gmail.com>
2023-05-25 15:51:28 -0400

    Route, controller, model set up

---
+++ lib/discuss_web/models/topic.ex
@@ -0,0 +1,14 @@
+defmodule Discuss.Topic do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "topics" do
+    field :email, :string
+  end
+
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:title])
+    |> validate_required(([:title]))
+  end
+end
