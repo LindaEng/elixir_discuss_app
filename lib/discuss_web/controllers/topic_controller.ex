36f0ba7f309f1da0130e5772f12a74e327c8d235
= <90lineng@gmail.com>
2023-06-14 14:15:44 -0400

    index function route added

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -4,7 +4,11 @@ defmodule DiscussWeb.TopicController do
   alias Discuss.Topic
   alias Discuss.Repo
 
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
 
+    render conn, "index.html", topics: topics
+  end
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
@@ -16,7 +20,10 @@ defmodule DiscussWeb.TopicController do
     changeset = Topic.changeset(%Topic{}, topic_params)
 
     case Repo.insert(changeset) do
-      {:ok, post} -> IO.inspect(post)
+      {:ok, post} ->
+        conn
+        |> put_flash(:info, "Topic Created")
+        |> redirect(to: Routes.topic_path(conn, :index))
       {:error, changeset} ->
         render conn, "new.html", changeset: changeset
     end
