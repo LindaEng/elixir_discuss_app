fabf39c8bd20607886bad8fe0b43ceae02db56b8
= <90lineng@gmail.com>
2023-06-22 15:24:32 -0400

    Refactor topic routes to use  macro
    This commit refactors the topic routes in the router file to utilize the  macro provided by Phoenix. The  macro allows for a more concise and standardized way of defining the RESTful routes for the TopicController.
    
    By using the  macro, the following routes are automatically generated:
    - GET /topics - maps to TopicController.index
    - POST /topics - maps to TopicController.create
    - GET /topics/new - maps to TopicController.new
    - GET /topics/:id - maps to TopicController.show
    - GET /topics/:id/edit - maps to TopicController.edit
    - PUT /topics/:id - maps to TopicController.update
    - DELETE /topics/:id - maps to TopicController.delete
    
    The  macro is applied to the scope to ensure that the browser pipeline is used for these routes.
    
    This refactoring simplifies the router configuration, improves readability, and adheres to the RESTful conventions for topic management.
    
    No issue or ticket is associated with this commit as it represents a decision made during development.

--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -17,12 +17,15 @@ defmodule DiscussWeb.Router do
   scope "/", DiscussWeb do
     pipe_through :browser
 
-    get "/", TopicController, :index
-    post "/topics", TopicController, :create
-    get "/topics/new", TopicController, :new
-    delete "/topics/:id/delete", TopicController, :delete
-    get "/topics/:id/edit", TopicController, :edit
-    put "/topics/:id", TopicController, :update
+    # get "/", TopicController, :index
+    # post "/topics", TopicController, :create
+    # get "/topics/new", TopicController, :new
+    # delete "/topics/:id/delete", TopicController, :delete
+    # get "/topics/:id/edit", TopicController, :edit
+    # put "/topics/:id", TopicController, :update
+
+    #built in helper by phoenix
+    resources "/", TopicController
   end
 
   # Other scopes may use custom stacks.
