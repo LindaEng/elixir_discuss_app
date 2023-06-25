72f64e0f177dfd99ab2879cbd8715aac64516d9a
= <90lineng@gmail.com>
2023-06-25 12:34:44 -0400

    pattern matched ueberauth map

--- lib/discuss_web/controllers/auth_controller.ex
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -2,7 +2,7 @@ defmodule DiscussWeb.AuthController do
   use DiscussWeb, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
-    
+  def callback(%{ueberauth_auth: auth} = conn, params) do
+
   end
 end
