21662723c0f4f70dd3820f3ec241880c5f6e2d5d
= <90lineng@gmail.com>
2023-07-05 20:37:57 -0400

    add timestamps to add comments migration file

--- priv/repo/migrations/20230706003429_add_comments.exs
+++ priv/repo/migrations/20230706003429_add_comments.exs
@@ -6,6 +6,8 @@ defmodule Discuss.Repo.Migrations.AddComments do
       add :content, :string
       add :user_id, references(:users)
       add :topic_id, references(:topics)
+
+      timestamps()
     end
   end
 end
