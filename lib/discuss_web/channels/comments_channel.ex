0a63d8e279e3390b3c026f529fd6f3acdd22d05e
= <90lineng@gmail.com>
2023-07-10 17:28:45 -0400

    adds text input form for comments

--- lib/discuss_web/channels/comments_channel.ex
+++ lib/discuss_web/channels/comments_channel.ex
@@ -1,14 +1,16 @@
 defmodule Discuss.CommentsChannel do
   use DiscussWeb, :channel
+  alias Discuss.Topic
+  alias Discuss.Repo
 
-  def join(name, _params, socket) do
-    IO.puts(name)
-    {:ok, %{hey: "HIII THERE"}, socket}
+  def join("comments:" <> topic_id, _params, socket) do
+    topic_id = String.to_integer(topic_id)
+    topic = Repo.get(Topic, topic_id)
+
+    {:ok, %{}, socket}
   end
 
-  def handle_in(name, message, socket) do
-    IO.puts("++++")
-    IO.puts(name)
+  def handle_in(name, %{"content" => content}, socket) do
     IO.inspect(message)
 
     {:reply, :ok, socket}
