3a7b77c21bc2b7faea6f06c632c88a53e2513f9a
= <90lineng@gmail.com>
2023-07-13 15:22:59 -0400

    Adds user email to each comment

--- lib/discuss_web/models/user.ex
+++ lib/discuss_web/models/user.ex
@@ -2,7 +2,7 @@ defmodule Discuss.User do
   use Ecto.Schema
   import Ecto.Changeset
 
-  @derive {Jason.Encoder, only: [:email, :provider, :token]}
+  @derive {Jason.Encoder, only: [:email]}
 
   schema "users" do
     field :email, :string
