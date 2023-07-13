64f2cd16201a32cebd5535c34c69e1fb12d531c5
= <90lineng@gmail.com>
2023-07-13 10:23:08 -0400

    add user to associate with comment

--- lib/discuss_web/channels/user_socket.ex
+++ lib/discuss_web/channels/user_socket.ex
@@ -5,8 +5,12 @@ defmodule DiscussWeb.UserSocket do
 
   @impl true
   def connect(%{"token" => token}, socket, _connect_info) do
-    IO.puts("THIS IS THE TOKEN ++++++")
-    IO.puts(token)
+    case Phoenix.Token.verify(socket, "key", token) do
+      {:ok, user_id} ->
+        {:ok, assign(socket, :user_id, user_id)}
+      {:error, _error} ->
+        :error
+    end
     {:ok, socket}
   end
 
--- lib/discuss_web/controllers/plugs/set_user.ex
+++ lib/discuss_web/controllers/plugs/set_user.ex
@@ -12,9 +12,7 @@ defmodule Discuss.Plugs.SetUser do
 
   def call(conn, _params) do
     user_id = get_session(conn, :user_id)
-    IO.inspect(user_id)
-    IO.puts("+++")
-    IO.inspect(conn.assigns)
+
     cond do
       user = user_id && Repo.get(User, user_id) ->
         assign(conn, :user, user)
